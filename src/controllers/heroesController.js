677da2e1d4b38cde652ba556215e11620ab132b8
morronepablo <morronepablo@gmail.com>
2023-03-06 10:51:59 -0300

    algunos ajustes

--- src/controllers/heroesController.js
+++ src/controllers/heroesController.js
@@ -2,40 +2,45 @@ const fs = require('fs');
 const path = require('path');
 
 const heroesFilePath = path.join(__dirname, '../data/heroes.json');
-const heroes = JSON.parse(fs.readFileSync(heroesFilePath, 'utf-8'));
+function getHeroes() {
+	return JSON.parse(fs.readFileSync(heroesFilePath, 'utf-8'));
+}
 
 const controller = {
-	// (get) Root - Mostrar todos los productos
+	// (get) Root - Mostrar todos los Heroes
 	index: (req, res) => {
-        const heroes = JSON.parse(fs.readFileSync(heroesFilePath, 'utf-8'));
+        const heroes = getHeroes();
 		res.render('heroes/heroes',{
 			 heroes
 		})
 	},
 
-	// (get) Detail - Detalle de un producto
+	// (get) Detail - Detalle de un Heroe
 	detail: (req, res) => {
 		const id = req.params.id;
+		const heroes = getHeroes();
 		const heroe = heroes.find(heroe => heroe.id == id);
 		res.render('heroe/heroe',{
 			heroe
 	   })
 	},
 
-	// (get) Create - Formulario para crear
+	// (get) Create - Formulario para crear Heroe
 	create: (req, res) => {
 		res.render('heroe/heroeFormCreate')
 	},
 	
 	// (post) Create -  Método para guardar la info
 	store: (req, res) => {
-		//Guradamos el producto
+		//Guradamos el heroe
 
+		const image = req.file ? req.file.filename : 'default-image.png';
+		const heroes = getHeroes();
 		let newHeroe = {
             id: heroes[heroes.length - 1].id + 1,
 			nombre: req.body.name,
 			bio: req.body.bio,
-			img: req.file.filename,
+			img: image,
 			aparicion: req.body.date,
 			casa: req.body.casa
 		}
@@ -44,9 +49,10 @@ const controller = {
 		res.redirect("/heroes")
 	},
 
-	// (get) Update - Formulario para editar
+	// (get) Update - Formulario para editar un Heroe
 	edit: (req, res) => {
 		const id = req.params.id;
+		const heroes = getHeroes();
 		const heroe = heroes.find(heroe => heroe.id == id);
 		
 		res.render('heroe/heroeFormEdit', {
@@ -55,41 +61,33 @@ const controller = {
 	},
 	// (put) Update - Método ara actualizar la info
 	update: (req, res) => {	
-		// Editamos el producto que llegó por parámetro su ID
-		let id = req.params.id;
-		let heroeToEdit = heroes.find(heroe => {
-			return heroe.id == id;
-		});
-
-		let editHeroe = {
-			id: id,
+		// Editamos el heroe que llegó por parámetro su ID
+		const id = req.params.id;
+		const heroes = getHeroes();
+		const heroeIndex = heroes.findIndex(heroe => heroe.id == id);
+		const image = req.file ? req.file.filename : heroes[heroeIndex].image;
+		heroes[heroeIndex] = {
+			...heroes[heroeIndex],
 			nombre: req.body.name,
 			bio: req.body.bio,
-			img: req.file ? req.file.filename : heroeToEdit.img,
+			img: image,
 			aparicion: req.body.date,
 			casa: req.body.casa
-		}
-		
-		// // Ya hemos modificado el array
-		heroes.forEach((heroe, index) => {
-			if(heroe.id == id) {
-				heroes[index] = editHeroe;
-			}
-		});
+		};
 
 		fs.writeFileSync(heroesFilePath, JSON.stringify(heroes, null, " "));
 		
 		res.redirect("/heroes")
 	},
 
-	// Delete - Delete one product from DB
+	// (delete) Delete - Método para eliminar un Heroe
 	destroy : (req, res) => {
-		//Eliminamos el producto que llegó por parámetro su ID
+		//Eliminamos el Heroe que llegó por parámetro su ID
 		let id = req.params.id;
-		
-		let finalHeroes = heroes.filter(heroe => heroe.id != id);
-		
-		fs.writeFileSync(heroesFilePath, JSON.stringify(finalHeroes, null, " "));
+		const heroes = getHeroes();
+		const heroeIndex = heroes.findIndex(heroe => heroe.id == id);
+		heroes.splice(heroeIndex, 1);
+		fs.writeFileSync(heroesFilePath, JSON.stringify(heroes, null, " "));
 
 		res.redirect("/heroes")	
 	}
